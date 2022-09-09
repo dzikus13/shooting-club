6c31726927d0acb415caa68a1f55d87601e2b3ad
Przemyslaw <milcarz.przemyslaw@gmail.com>
2022-09-09 11:54:42 +0200

    inital version of pricelists models

--- pricelists/models.py
+++ pricelists/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class pricelist1(models.Model):
+    gun_name = models.CharField(max_length=255)
+    caliber = models.CharField(max_length=20)
+    cb_price = models.CharField(max_length=7) #club member
+    ncb_price = models.CharField(max_length=7) #non club member
+
+class pricelist2(models.Model):
+    gun_name = models.CharField(max_length=255)
+    caliber = models.CharField(max_length=20)
+    cb_price = models.DecimalField(max_digits=6, decimal_places=2) #club member
+    ncb_price = models.DecimalField(max_digits=6, decimal_places=2) #non club member
+
+class pricelist3(models.Model):
+    gun_name = models.CharField(max_length=255)
+    caliber = models.CharField(max_length=20)
+    cb_price = models.FloatField() #club member
+    ncb_price = models.FloatField() #non club member
