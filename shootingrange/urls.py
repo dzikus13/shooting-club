b40244f528f0538c69e7cef24884b1267cbdb6d5
Przemyslaw <milcarz.przemyslaw@gmail.com>
2022-09-15 09:15:01 +0200

    pricelist updated, showing pricelist in a table.

--- shootingrange/urls.py
+++ shootingrange/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from pricelists.views import pricelist
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('pricelist/', pricelist)
 ]
