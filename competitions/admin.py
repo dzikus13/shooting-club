a00bf3a0ec0e92bf0f69f1cf1fdd1c5e0a84a340
DominikDodo <spoke111klone@gmail.com>
2022-09-22 09:40:11 +0200

    Admin User is able to manage tournament

---
+++ competitions/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+
+# Register your models here.
+
+from .models import Tournament,Participant,Pricing,Competition,Prize,Schedule,Score
+
+admin.site.register(Tournament)
+admin.site.register(Participant)
+admin.site.register(Pricing)
+admin.site.register(Competition)
+admin.site.register(Prize)
+admin.site.register(Schedule)
+admin.site.register(Score)
---
+++ competitions/models.py
@@ -0,0 +1,44 @@
+from unicodedata import name
+from django.contrib.auth import get_user_model
+from django.db import models
+from allauth.account.signals import user_logged_in
+
+User = get_user_model()
+
+def user_logged_in_receiver(request, user, **kwargs):
+    print(request)
+    print(user)
+
+user_logged_in.connect(user_logged_in_receiver, sender=User)
+
+class Tournament(models.Model):
+    name = models.CharField(max_length=255)
+    date = models.DateTimeField()
+    description = models.TextField()
+
+class Participant(models.Model):
+    name = models.CharField(max_length=255)
+
+class Pricing(models.Model):
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+
+class Competition(models.Model):
+    competition_name = models.CharField(max_length=255)
+
+class Prize(models.Model):
+    description = models.TextField()
+    place = models.DecimalField(max_digits=3, decimal_places=0)
+    competition=models.ForeignKey(Competition, on_delete=models.CASCADE)
+
+class Schedule(models.Model):
+    participant=models.ForeignKey(Participant, on_delete=models.CASCADE)
+    competition=models.ForeignKey(Competition, on_delete=models.CASCADE)
+    schedule_date = models.DateTimeField()
+
+class Score(models.Model):
+    score = models.TextField()
+    participant = models.ForeignKey(Participant, on_delete=models.CASCADE)
+    competition = models.ForeignKey(Competition, on_delete=models.CASCADE)
+    
+
+
