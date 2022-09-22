0a923f03582e8565693c78a17b55494d17e13852
DominikDodo <112878825+DominikDodo@users.noreply.github.com>
2022-09-22 09:43:14 +0200

    Update admin.py

--- competitions/admin.py
+++ competitions/admin.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 
 # Register your models here.
 
-
 from .models import Tournament,Participant,Pricing,Competition,Prize,Schedule,Score
 
 admin.site.register(Tournament)
@@ -12,9 +11,5 @@ admin.site.register(Competition)
 admin.site.register(Prize)
 admin.site.register(Schedule)
 admin.site.register(Score)
-
-from .models import Tournament
-
-admin.site.register(Tournament)
 
 
