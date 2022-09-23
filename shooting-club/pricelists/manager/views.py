6f6e79119d3898bbfbf0a9f2f512828b40ea44f5
Mateusz <mateuszsplonskowski@gmail.com>
2022-09-23 09:19:34 +0200

    New version without pycache

--- shooting-club/pricelists/manager/views.py
+++ shooting-club/pricelists/manager/views.py
@@ -1,5 +1,25 @@
 from django.shortcuts import render
 
 
-def base(request):
+def index(request):
     return render(request, "manager/index.html")
+
+
+def about(request):
+    return render(request, "manager/about.html")
+
+
+def contact(request):
+    return render(request, "manager/contact.html")
+
+
+def home(request):
+    return render(request, "manager/home.html")
+
+
+def pages(request):
+    return render(request, "manager/pages.html")
+
+
+def services(request):
+    return render(request, "manager/services.html")
