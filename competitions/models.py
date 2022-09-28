1b27fa41fb76575a12abef540d1e680f1a036b91
DominikDodo <spoke111klone@gmail.com>
2022-09-28 10:07:52 +0200

    Templates for tournaments view with help of Biedok

--- competitions/models.py
+++ competitions/models.py
@@ -15,30 +15,43 @@ class Tournament(models.Model):
     name = models.CharField(max_length=255)
     date = models.DateTimeField()
     description = models.TextField()
+    def __str__(self):
+        return self.name 
 
 class Participant(models.Model):
     name = models.CharField(max_length=255)
+    def __str__(self):
+        return self.name 
 
 class Pricing(models.Model):
     price = models.DecimalField(max_digits=8, decimal_places=2)
+    def __str__(self):
+        return self.name
 
 class Competition(models.Model):
     competition_name = models.CharField(max_length=255)
+    def __str__(self):
+        return self.name
 
 class Prize(models.Model):
     description = models.TextField()
     place = models.DecimalField(max_digits=3, decimal_places=0)
     competition=models.ForeignKey(Competition, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.name
 
 class Schedule(models.Model):
     participant=models.ForeignKey(Participant, on_delete=models.CASCADE)
     competition=models.ForeignKey(Competition, on_delete=models.CASCADE)
     schedule_date = models.DateTimeField()
+    def __str__(self):
+        return self.name
 
 class Score(models.Model):
     score = models.TextField()
     participant = models.ForeignKey(Participant, on_delete=models.CASCADE)
     competition = models.ForeignKey(Competition, on_delete=models.CASCADE)
+    
     
 
 
--- competitions/views.py
+++ competitions/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from .models import Tournament
 # Create your views here.
+
+def tournament(request): # get all tournaments
+    t = Tournament.objects.all()
+    return render(request, "tournament.html", {"t":t})
